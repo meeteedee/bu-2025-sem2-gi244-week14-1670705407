93014e18b1d95ea74094094f7d2b5f5ec8a4e413
meeteedee <meepoohfisho@gmail.com>
2026-04-24 16:53:44 +0700

    done

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -27,22 +27,47 @@ public class MainManager : MonoBehaviour
     [System.Serializable]
     class SaveData
     {
-
+        public Color TeamColor;
+        public string MyName;
     }
 
     public void SaveColor()
     {
-        PlayerPrefs.SetFloat("TeamColor.r", TeamColor.r);
+        string folder = Application.persistentDataPath; //"C:\\Users\\Administrator\\Desktop";
+        string fileName = "saveData.json";
+        string fullPath = Path.Combine(folder, fileName);
+        
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+        data.MyName = "Big";
+        string j = JsonUtility.ToJson(data);
+        Debug.Log(j);
+        Debug.Log(fullPath);
+        
+        File.WriteAllText(fullPath, j);
+        /*PlayerPrefs.SetFloat("TeamColor.r", TeamColor.r);
         PlayerPrefs.SetFloat("TeamColor.g", TeamColor.g);
         PlayerPrefs.SetFloat("TeamColor.b", TeamColor.b);
-        PlayerPrefs.SetFloat("TeamColor.a", TeamColor.a);
+        PlayerPrefs.SetFloat("TeamColor.a", TeamColor.a);*/
     }
 
     public void LoadColor()
     {
-        TeamColor.r = PlayerPrefs.GetFloat("TeamColor.r");
+        string folder = Application.persistentDataPath; //"C:\\Users\\Administrator\\Desktop";
+        string fileName = "saveData.json";
+        string fullPath = Path.Combine(folder, fileName);
+        if (File.Exists(fullPath))
+        {
+            string j = File.ReadAllText(fullPath);
+            var data = JsonUtility.FromJson<SaveData>(j);
+            TeamColor = data.TeamColor;
+            
+        }
+        //string j = PlayerPrefs.GetString("saveData");
+        
+        /*TeamColor.r = PlayerPrefs.GetFloat("TeamColor.r");
         TeamColor.g = PlayerPrefs.GetFloat("TeamColor.g");
         TeamColor.b = PlayerPrefs.GetFloat("TeamColor.b");
-        TeamColor.a = PlayerPrefs.GetFloat("TeamColor.a");
+        TeamColor.a = PlayerPrefs.GetFloat("TeamColor.a");*/
     }
 }
